0bd38e6ed268c75226fea27b57efdb756ec2d185
naumanm2 <max.naumanen@aalto.fi>
2021-11-13 11:17:02 +0200

    aboutmewrapper

--- src/pages/aboutme.js
+++ src/pages/aboutme.js
@@ -1,26 +1,27 @@
-import React from 'react'
-import AboutMe from '../components/aboutme'
-import Footer from '../components/footer'
-import Hamburger from '../components/hamburger'
-import Header from '../components/header'
-import Resume from '../components/resume'
-import Sidebar from '../components/sidebar'
+import React from "react";
+import AboutMe from "../components/aboutme";
+import Footer from "../components/footer";
+import Hamburger from "../components/hamburger";
+import Header from "../components/header";
+import Resume from "../components/resume";
+import Sidebar from "../components/sidebar";
 
-import '../styles.scss'
+import "../styles.scss";
 
 const aboutme = () => {
-    return (
-        <div>
-            <Header />
-            <Hamburger />
-            <Sidebar active="aboutme"/>
-                <div style={{ height: '70vh', overflowY: 'scroll'}}>
-                <AboutMe />
-                <Resume />
-            </div>
-            <Footer />
-        </div>
-    )
-}
+  return (
+    <div>
+      <Header />
+      <Hamburger />
+      <Sidebar active="aboutme" />
+      {/* <div style={{ height: "80vh", overflowY: "scroll" }}> */}
+      <div className="aboutmewrapper">
+        <AboutMe />
+        <Resume />
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
-export default aboutme
+export default aboutme;
